0bc1babd6243e331f1b17d9fcbfeedbd522f40e2
ronierisonmaciel <ronierison.maciel@gmail.com>
2022-02-11 10:14:45 -0300

    shell script

---
+++ script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Qual seu nome de usuário:"
+whoami
+echo "Informação da hora atual e tempo que o computador está ligado:"
+uptime
+echo "O script está executando do diretório:"
+pwd
